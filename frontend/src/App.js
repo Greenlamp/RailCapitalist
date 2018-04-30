dfeb4c7e9491798ea64023e88457ebc0414d7112
Greenlamp <gabriel.knuts@gmail.com>
2018-04-30 18:57:06 +0200

    Add, update and delete working client sided

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
 import {Route, Switch, BrowserRouter} from 'react-router-dom';
+
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import railApp from "./reducers";
+
 import RailCapitalist from "./components/RailCapitalist";
 import NotFound from "./components/NotFound";
+
+let store = createStore(railApp);
 
 class App extends Component {
   render() {
   return (
-    <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={RailCapitalist} />
-      <Route component={NotFound} />
-    </Switch>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={RailCapitalist} />
+        <Route component={NotFound} />
+      </Switch>
+      </BrowserRouter>
+    </Provider>
   );
   }
 }
--- frontend/src/components/RailCapitalist.jsx
+++ frontend/src/components/RailCapitalist.jsx
@@ -1,16 +1,87 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {notes} from "../actions";
 
 
-export default class PonyNote extends Component {
+class PonyNote extends Component {
+  state = {
+    text: "",
+    updateNoteId: null,
+  }
+
+  resetForm = () => {
+    this.setState({text: "", updateNoteId: null});
+  }
+
+  selectForEdit = (id) => {
+    let note = this.props.notes[id];
+    this.setState({text: note.text, updateNoteId: id});
+  }
+
+  submitNote = (e) => {
+    e.preventDefault();
+    if (this.state.updateNoteId === null) {
+      this.props.addNote(this.state.text);
+    } else {
+      this.props.updateNote(this.state.updateNoteId, this.state.text);
+    }
+    this.resetForm();
+  }
+
   render() {
-  return (
-    <div>
-    <h2>Welcome to PonyNote!</h2>
-    <p>
-      <Link to="/contact">Click Here</Link> to contact us!
-    </p>
-    </div>
-  )
+    console.log(this.noteList);
+    return (
+      <div>
+        <h2>Welcome to PonyNote!</h2>
+        <hr />
+
+        <h3>Notes</h3>
+        <p>ID: {this.state.updateNoteId}</p>
+        <table>
+          {this.props.notes.map((note, id) => (
+            <tr key={`note_${id}`}>
+              <td>{id}</td>
+              <td>{note.text}</td>
+              <td><button onClick={() => this.selectForEdit(id)}>edit</button></td>
+              <td><button onClick={() => this.props.deleteNote(id)}>delete</button></td>
+            </tr>
+          ))}
+        </table>
+        <h3>Add new note</h3>
+        <form onSubmit={this.submitNote}>
+          <input
+            value={this.state.text}
+            placeholder="Enter note here..."
+            onChange={(e) => this.setState({text: e.target.value})}
+            required />
+          <input type="submit" value="Save Note" />
+          <button onClick={this.resetForm}>Reset</button>
+        </form>
+      </div>
+    )
   }
 }
+
+
+const mapStateToProps = state => {
+  return {
+    notes: state.notes,
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addNote: (text) => {
+      dispatch(notes.addNote(text));
+    },
+    updateNote: (id, text) => {
+      dispatch(notes.updateNote(id, text));
+    },
+    deleteNote: (id) => {
+      dispatch(notes.deleteNote(id));
+    },
+  }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(PonyNote);
