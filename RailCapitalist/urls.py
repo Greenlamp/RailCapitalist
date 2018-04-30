42bc7661cad307b58d33d172b0d3f25d6f42eabc
Greenlamp <gabriel.knuts@gmail.com>
2018-04-30 17:21:00 +0200

    Integration of React: working

--- RailCapitalist/urls.py
+++ RailCapitalist/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    #url(r'^admin/', admin.site.urls),
     url(r'^', TemplateView.as_view(template_name="index.html")),
 ]
