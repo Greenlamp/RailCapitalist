932af44872e76a68ad7c7e1c044f7cfddc036bc5
Greenlamp <gabriel.knuts@gmail.com>
2018-05-14 21:52:30 +0200

    redux thunk useless

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,14 @@ import React, { Component } from 'react';
 import {Route, Switch, BrowserRouter} from 'react-router-dom';
 
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
+import { createStore, applyMiddleware, compose } from "redux";
 
 import railApp from "./reducers";
 
 import RailCapitalist from "./components/RailCapitalist";
 import NotFound from "./components/NotFound";
-import {compose} from "redux";
 
-let middles = [thunk,]
+let middles = []
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const enhancer = composeEnhancers(
